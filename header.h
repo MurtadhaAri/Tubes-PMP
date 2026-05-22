57726ee0a487893149d9d6683d38d315b37ab9a0
MurtadhaAri <murtadhaarlkari@gmail.com>
2026-05-23 01:08:03 +0700

    Header
    Header

---
+++ header.h
@@ -0,0 +1,23 @@
+#ifndef HEADER_H   /* Include guard */
+#define HEADER_H
+
+typedef struct node {
+    int id_barang;
+    char nama_barang[100];
+    char kategori [20];
+    int jumlah_stock;
+    short int status;
+    char PIC[30];
+    char PemilikBarang[30];
+    struct node * next;
+}node;
+
+void  tambah(node ** head, int num); // tambah di butnut, parameter sesuaikan (nigo)
+void Del (node **head, int num); // delete at index (moti)
+void Find (node **head, int num); // find by id (bang sam)
+void Display (node **head); // all (ari)
+void UpdateStock (node **head); // all (nigo)
+void updateStatus (node **head); // all (Update Status)
+void Disp_ringkas (node **head); // all (bang sam)
+
+#endif
