2f180f5a67b4f9c38c4235e21efe8c760f80f1bd
Timothy <hy.timothy@gmail.com>
2026-06-19 11:53:30 +0700

    Implement update_status_rusak function
    Add function to update item status to 'damaged' based on ID.

---
+++ update_status.c
@@ -0,0 +1,23 @@
+#include "header.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void update_status_rusak(node **head) {
+    int id;
+    printf("Masukkan ID item yang mau diupdate rusak: ");
+    scanf("%d", &id);
+
+    node *temp = *head; // Start from the head of the list
+
+    while (temp != NULL) {
+        if (temp->id_barang == id) {
+            temp->status = 3;
+            return; 
+        }
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        printf("Item dengan ID %d tidak ditemukan.\n", id);
+    }
+}
