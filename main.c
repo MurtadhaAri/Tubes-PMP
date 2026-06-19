21aeee7f881a09572163a84885bcb6e1c3ca6251
Timothy <hy.timothy@gmail.com>
2026-06-19 10:03:38 +0700

    Set up UART and main loop in main.c
    Initialize UART and set up main loop for Arduino Uno.

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <util/delay.h>
+#include "uart.h"
 #include "header.h"
 
-int main () {
-    node *head == NULL;
-    // Masukkan isi main
+int main(void) {
+    // Inisialisasi UART dengan baud rate 9600
+    uart_init(9600);
+    printf("\n--- Arduino Uno C Environment Ready ---\n");
+    while (1) {
+        // taro code disini
+        int angka1, angka2;
+        
+        
+        
+
+
+        // batas nulis code
+        _delay_ms(1000);
+    }
     return 0;
 }
