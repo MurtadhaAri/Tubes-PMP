89a18bed75da61b188f5cb60a004a67073c6c0d5
MurtadhaAri <murtadhaarlkari@gmail.com>
2026-06-20 20:54:37 +0700

    Updated tambah.c

--- tambah.c
+++ tambah.c
@@ -11,56 +11,48 @@ void tambah(node ** head) {
     uint16_t eeprom = 1024;
     char id_buffer[12];   
     uint16_t num = 0;     
-
     uint8_t temp8;
     uint16_t temp16;       
-    
     uint8_t id_valid = 0;
     uint8_t verif_scan;
     int i;
     uint32_t cek_nilai;
     node *current;
     node *newNode;
-    
     uint16_t current_count = 0;
     node *cek_node;
-
     cek_node = *head;
-    while (cek_node != NULL) {
+    while(cek_node != NULL) {
         current_count++;
         cek_node = cek_node->next;
     }
 
-    if (((current_count + 1) * sizeof(node)) > eeprom) {
+    if(((current_count + 1) * sizeof(node)) > eeprom) {
         printf_P(PSTR("Tidak dapat menyimpan data lebih dari ini\n\n"));
         return;
     }
 
-    while (!id_valid) {
+    while(!id_valid) {
         printf_P(PSTR("Masukkan ID Barang Baru: "));
         if (fgets(id_buffer, sizeof(id_buffer), stdin) == NULL) continue;
-
         id_buffer[strcspn(id_buffer, "\r\n")] = '\0';
-
         if (strlen(id_buffer) == 0) {
             printf_P(PSTR("ID tidak boleh kosong\n\n"));
             continue;
         }
-
         id_valid = 1; 
-        for (i = 0; id_buffer[i] != '\0'; i++) {
-            if (!isdigit((unsigned char)id_buffer[i])) {
+        for(i = 0; id_buffer[i] != '\0'; i++) {
+            if(!isdigit((unsigned char)id_buffer[i])) {
                 id_valid = 0;
                 break;
             }
         }
-
-        if (!id_valid) {
+        if(!id_valid) {
             printf_P(PSTR("ID harus berupa angka saja\n\n"));
         } else {
             cek_nilai = strtoul(id_buffer, NULL, 10);
 
-            if (cek_nilai > 65535) {
+            if(cek_nilai > 65535) {
                 printf_P(PSTR("Maksimal ID 65535\n\n"));
                 id_valid = 0;
             } else {
@@ -70,8 +62,8 @@ void tambah(node ** head) {
     }
 
     current = *head;
-    while (current != NULL) {
-        if (current->id_barang == num) {
+    while(current != NULL) {
+        if(current->id_barang == num) {
             printf_P(PSTR("ID %hu sudah ada\n\n"), num);
             return;
         }
@@ -79,7 +71,7 @@ void tambah(node ** head) {
     }
 
     newNode = (node*)malloc(sizeof(node));
-    if (newNode == NULL) {
+    if(newNode == NULL) {
         printf_P(PSTR("Kapasitas memori hampir habis\n\n"));
         return;
     }
@@ -95,7 +87,7 @@ void tambah(node ** head) {
         verif_scan = scanf("%hhu", &temp8); 
         while(getchar() != '\n');
 
-        if (verif_scan == 1 && (temp8 == 0 || temp8 == 1 || temp8 == 2)) {
+        if(verif_scan == 1 && (temp8 == 0 || temp8 == 1 || temp8 == 2)) {
             newNode->kategori = temp8; 
             break;
         } else {
@@ -108,7 +100,7 @@ void tambah(node ** head) {
         verif_scan = scanf("%hhu", &temp8);
         while(getchar() != '\n');
 
-        if (verif_scan == 1 && (temp8 == 0 || temp8 == 1 || temp8 == 2)) { 
+        if(verif_scan == 1 && (temp8 == 0 || temp8 == 1 || temp8 == 2)) { 
             newNode->status = temp8;
             break;
         } else {
@@ -116,7 +108,7 @@ void tambah(node ** head) {
         }
     }
 
-    if (newNode->status == 1 || newNode->status == 2){
+    if(newNode->status == 1 || newNode->status == 2){
         while(1){
             printf_P(PSTR("Masukkan Jumlah Stok: "));
             verif_scan = scanf("%hu", &temp16);
@@ -136,24 +128,18 @@ void tambah(node ** head) {
     printf_P(PSTR("Masukkan Lokasi Penyimpanan: "));
     fgets(newNode->lokasi_penyimpanan, sizeof(newNode->lokasi_penyimpanan), stdin);
     newNode->lokasi_penyimpanan[strcspn(newNode->lokasi_penyimpanan, "\r\n")] = '\0';
-
-     
-
     printf_P(PSTR("Masukkan PIC: "));
     fgets(newNode->PIC, sizeof(newNode->PIC), stdin);
     newNode->PIC[strcspn(newNode->PIC, "\r\n")] = '\0';
-
     printf_P(PSTR("Masukkan Pemilik Barang: "));
     fgets(newNode->PemilikBarang, sizeof(newNode->PemilikBarang), stdin);
     newNode->PemilikBarang[strcspn(newNode->PemilikBarang, "\r\n")] = '\0';
-
     newNode->next = NULL;
-
-    if (*head == NULL) {
+    if(*head == NULL) {
         *head = newNode;
     } else {
         current = *head;
-        while (current->next != NULL) {
+        while(current->next != NULL) {
             current = current->next;
         }
         current->next = newNode;
