16878e34f0566dc8c75276c027019a7512609bcb
Nicholas <nicholas.simarmata@gmail.com>
2026-06-20 14:32:31 +0700

    added pembatas size tambah.c

--- tambah.c
+++ tambah.c
@@ -6,20 +6,35 @@
 #include <ctype.h>        
 #include <avr/pgmspace.h> 
 
+#define EEPROM_SIZE 1024
+
 void tambah(node ** head) {
     char id_buffer[12];   
     uint16_t num = 0;     
-    
-    // PERBAIKAN: Pemisahan variabel agar scanf %hhu dan %hu akurat
+
     uint8_t temp8;
     uint16_t temp16;       
     
     uint8_t id_valid = 0;
     uint8_t scan_res;
-    int i; // C90 Standard
+    int i;
     uint32_t cek_nilai;
     node *current;
     node *newNode;
+    
+    uint16_t current_count = 0;
+    node *cek_node;
+
+    cek_node = *head;
+    while (cek_node != NULL) {
+        current_count++;
+        cek_node = cek_node->next;
+    }
+
+    if (((current_count + 1) * sizeof(node)) > EEPROM_SIZE) {
+        printf_P(PSTR("Tidak dapat menyimpan data lebih dari ini\n\n"));
+        return;
+    }
 
     while (!id_valid) {
         printf_P(PSTR("Masukkan ID Barang Baru: "));
@@ -77,7 +92,7 @@ void tambah(node ** head) {
 
     while (1) {
         printf_P(PSTR("Masukkan Kategori (0 = Komponen, 1 = Alat, 2 = Lainnya): "));
-        scan_res = scanf("%hhu", &temp8); // Menggunakan %hhu untuk 8-bit
+        scan_res = scanf("%hhu", &temp8); 
         while(getchar() != '\n');
 
         if (scan_res == 1 && (temp8 == 0 || temp8 == 1 || temp8 == 2)) {
@@ -90,7 +105,7 @@ void tambah(node ** head) {
 
     while(1){
         printf_P(PSTR("Masukkan Jumlah Stok: "));
-        scan_res = scanf("%hu", &temp16); // Menggunakan %hu karena targetnya 16-bit
+        scan_res = scanf("%hu", &temp16);
         while(getchar() != '\n'); 
         if(scan_res == 1 && temp16 > 0 && temp16 <= 65535){
             newNode->jumlah_stock = temp16;
@@ -106,7 +121,7 @@ void tambah(node ** head) {
 
     while (1) {
         printf_P(PSTR("Masukkan Status (1 = Tersedia, 0 = Habis): "));
-        scan_res = scanf("%hhu", &temp8); // Menggunakan %hhu untuk 8-bit
+        scan_res = scanf("%hhu", &temp8);
         while(getchar() != '\n');
 
         if (scan_res == 1 && (temp8 == 0 || temp8 == 1)) { 
