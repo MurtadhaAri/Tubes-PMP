bd3d4a576ef8771320bce75cc50625e50141035b
MurtadhaAri <murtadhaarlkari@gmail.com>
2026-06-20 20:44:58 +0700

    Update load.c

--- load.c
+++ load.c
@@ -6,14 +6,14 @@
 
 void load(node **head) {
     uint8_t total_barang;
-    uint16_t alamat_eeprom; // PERBAIKAN: Harus 16-bit untuk mencapai 1024 bytes
+    uint16_t alamat_eeprom;
     uint8_t i;
     node *newNode;
     node *current;
 
     total_barang = eeprom_read_byte((const uint8_t*)0);
     
-    if (total_barang == 255 || total_barang == 0) {
+    if(total_barang == 255 || total_barang == 0) {
         printf_P(PSTR("EEPROM kosong. Memulai sistem dari awal.\n"));
         return;
     }
@@ -21,9 +21,9 @@ void load(node **head) {
     alamat_eeprom = 1;
     printf_P(PSTR("Menemukan %u barang di EEPROM. Memuat data...\n"), total_barang);
 
-    for (i = 0; i < total_barang; i++) {
+    for(i = 0; i < total_barang; i++) {
         newNode = (node*)malloc(sizeof(node));
-        if (newNode == NULL) {
+        if(newNode == NULL) {
             // PERBAIKAN: Typo printfP diubah menjadi printf_P
             printf_P(PSTR("Memori SRAM penuh saat memuat data!\n"));
             return;
@@ -33,9 +33,11 @@ void load(node **head) {
         
         newNode->next = NULL;
 
-        if (*head == NULL) {
+        if(*head == NULL) 
+        {
             *head = newNode;
-        } else {
+        } 
+        else {
             current = *head;
             while (current->next != NULL) {
                 current = current->next;
