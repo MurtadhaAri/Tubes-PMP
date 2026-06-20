69f93400f0a585c1136732b6fe9dcbe6ec6178cf
MurtadhaAri <murtadhaarlkari@gmail.com>
2026-06-20 20:57:07 +0700

    Updated update_status.c

--- update_status.c
+++ update_status.c
@@ -5,27 +5,25 @@
 #include <avr/pgmspace.h>
 
 void update_status(node **head) {
-    if (*head == NULL) {
+    if(*head == NULL) {
         printf_P(PSTR("Tidak ada barang di dalam database\n\n"));
         return;
     }
-
     uint16_t id;
     printf_P(PSTR("Masukkan ID item yang mau diupdate statusnya: "));
     scanf("%hu", &id);
-    while (getchar() != '\n');
-
+    while(getchar() != '\n');
     node *temp = *head;
 
-    while (temp != NULL) {
-        if (temp->id_barang == id) {
+    while(temp != NULL) {
+        if(temp->id_barang == id) {
             printf_P(PSTR("Barang ditemukan: %s\n"), temp->nama_barang);
             printf_P(PSTR("Status saat ini : "));
 
-            if (temp->status == 1) {
+            if(temp->status == 1) {
                 printf_P(PSTR("Tersedia\n"));
                 printf_P(PSTR("Apakah ingin mengubah status barang menjadi Rusak? (Ketik 1 jika ya): "));
-            } else if (temp->status == 2) {
+            } else if(temp->status == 2) {
                 printf_P(PSTR("Rusak\n"));
                 printf_P(PSTR("Apakah ingin mengubah status barang menjadi Tersedia? (Ketik 1 jika ya): "));
             } else {
@@ -35,10 +33,10 @@ void update_status(node **head) {
 
             uint8_t pilihan_status = 0;
             uint8_t verif_scan = scanf("%d", &pilihan_status);
-            while (getchar() != '\n');
+            while(getchar() != '\n');
 
-            if (verif_scan == 1 && pilihan_status == 1) {
-                if (temp->status == 1) {
+            if(verif_scan == 1 && pilihan_status == 1) {
+                if(temp->status == 1) {
                     temp->status = (uint8_t)2;
                 } else {
                     temp->status = (uint8_t)1;
@@ -47,7 +45,6 @@ void update_status(node **head) {
             } else {
                 printf_P(PSTR("Status tidak berubah.\n\n"));
             }
-            
             return;
         }
         temp = temp->next;
