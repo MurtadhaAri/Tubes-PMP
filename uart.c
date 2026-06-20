cd3b3f2a2dc71094eac409c26c4c5f4c489175e8
MurtadhaAri <murtadhaarlkari@gmail.com>
2026-06-20 20:55:53 +0700

    Updated uart.c

--- uart.c
+++ uart.c
@@ -1,48 +1,32 @@
 #include "uart.h"
 #include <avr/io.h>
 
-// Fungsi untuk inisialisasi UART
 void uart_init(unsigned long baud) {
-    // Menghitung UBRR (Baud Rate Register)
     unsigned int ubrr = F_CPU / 16 / baud - 1;
     UBRR0H = (unsigned char)(ubrr >> 8);
     UBRR0L = (unsigned char)ubrr;
-    
-    // Mengaktifkan Receiver dan Transmitter
     UCSR0B = (1 << TXEN0) | (1 << RXEN0);
-    
-    // Mengatur format frame: 8 data bit, 1 stop bit
     UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
-
-    // Menghubungkan printf dan scanf ke UART
     static FILE uart_output = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
     static FILE uart_input = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
-    
     stdout = &uart_output;
     stdin = &uart_input;
 }
-
-// Fungsi untuk mengirimkan karakter
 int uart_putchar(char c, FILE *stream) {
     if (c == '\n') {
-        uart_putchar('\r', stream); // Tambahkan Carriage Return untuk Serial Monitor
+        uart_putchar('\r', stream);
     }
-    while (!(UCSR0A & (1 << UDRE0))); // Tunggu buffer kosong
+    while (!(UCSR0A & (1 << UDRE0)));
     UDR0 = c;
     return 0;
 }
-
-// Fungsi untuk menerima karakter
 int uart_getchar(FILE *stream) {
-    while (!(UCSR0A & (1 << RXC0))); // Tunggu data masuk
+    while (!(UCSR0A & (1 << RXC0)));
     char c = UDR0;
-    
-    // Echo back (menampilkan kembali apa yang diketik pengguna di Serial Monitor)
     if (c == '\r') {
         uart_putchar('\n', stdout);
     } else {
         uart_putchar(c, stdout);
     }
-    
     return c;
 }
