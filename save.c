673300ea6f35abce420b091760d0422ab3d6b7cc
MurtadhaAri <murtadhaarlkari@gmail.com>
2026-06-20 20:50:56 +0700

    Updated save.c

--- save.c
+++ save.c
@@ -5,25 +5,20 @@
 
 void save(node **head) {
     uint8_t total_barang = 0;
-    uint16_t alamat_eeprom = 1; // PERBAIKAN: Harus 16-bit
+    uint16_t alamat_eeprom = 1;
     node *current = *head;
-
     printf_P(PSTR("\nMenyimpan data ke EEPROM...\n"));
-
     while (current != NULL) {
         if (alamat_eeprom + sizeof(node) > 1023) {
-            printf_P(PSTR("[WARNING] Kapasitas fisik EEPROM (1KB) sudah penuh!\n"));
+            printf_P(PSTR("Kapasitas fisik EEPROM (1KB) sudah penuh!\n"));
             printf_P(PSTR("Hanya %u barang pertama yang berhasil diselamatkan.\n"), total_barang);
             break; 
         }
-
         eeprom_update_block((const void*)current, (void*)alamat_eeprom, sizeof(node));
-        
         alamat_eeprom += sizeof(node); 
         total_barang++;
         current = current->next;
     }
-
     eeprom_update_byte((uint8_t*)0, total_barang);
-    printf_P(PSTR("Berhasil! %u barang tersimpan permanen.\n"), total_barang);
+    printf_P(PSTR("Berhasil %u barang tersimpan permanen.\n"), total_barang);
 }
